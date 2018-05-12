1431057e5f27fcf2a155fa4f364cdfbda0167b5f
Kathy Nguyen <kathyvng@gmail.com>
2018-05-12 08:05:58 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,16 +1,2 @@
-'''
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-# Position 1: Left
-board[0, 3]
-
-# Position 2: Middle
-board[1, 4]
-
-# Position 3: Right
-board[2, 9]
-
-# Move X to the Position 2, Middle
-board[1, 4] = "X"
-board #=> [" ", "X", " "]
-'''
